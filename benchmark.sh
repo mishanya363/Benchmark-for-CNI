c061dd9950e8c2fba119b1945cfd26581c9e4ce0
mishanya363 <162026920+mishanya363@users.noreply.github.com>
2024-03-04 01:06:16 +0300

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for run in 1 2 3
 do
-    RESULT_FILE_PREFIX="test $run"
+    RESULT_FILE_PREFIX="test_$run"
     ./knb -v -cn worker-node -sn worker-node-2 -o data -f results/$RESULT_FILE_PREFIX.knbdata --name "Ubuntu $DISTRIBUTION - Kernel $KERNEL - Run $run"
     ./knb -fd results/$RESULT_FILE_PREFIX.knbdata -o ibdbench -f results/tsv/$RESULT_FILE_PREFIX.tsv
     ./knb -fd results/$RESULT_FILE_PREFIX.knbdata
