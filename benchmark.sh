ba769b4298fdb799d2810746cc0c8d4ba58279d8
mishanya363 <mihanoid369@gmail.com>
2024-03-23 14:02:24 +0300

    F

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,7 @@ touch /root/Benchmark-for-CNI/results/csv/results.csv
 for run in $(seq 1 3)
 do
     RESULT_FILE_PREFIX="test_$run"
-    ./knb -v -cn worker-node -sn worker-node-2 -ot p2ptcp,p2pudp -o data -f /root/Benchmark-for-CNI/results/$RESULT_FILE_PREFIX.knbdata
+    ./knb -v -cn worker-node-1 -sn worker-node-2 -ot p2ptcp,p2pudp -o data -f /root/Benchmark-for-CNI/results/$RESULT_FILE_PREFIX.knbdata
     ./knb -fd /root/Benchmark-for-CNI/results/$RESULT_FILE_PREFIX.knbdata -o ibdbench >> /root/Benchmark-for-CNI/results/csv/results.csv
     ./knb -fd /root/Benchmark-for-CNI/results/$RESULT_FILE_PREFIX.knbdata
 done
