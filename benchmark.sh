a55222facb2bdb08bd7fb039206412649496368f
mishanya363 <mihanoid369@gmail.com>
2024-03-23 12:17:55 +0300

    F

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-for run in 1 2 3
+rm /root/Benchmark-for-CNI/results/csv/results.csv
+touch /root/Benchmark-for-CNI/results/csv/results.csv
+for run in $(seq 1 3)
 do
-    RESULT_FILE_PREFIX="test $run"
-    ./knb -v -cn worker-node -sn worker-node-2 -o data -f results/$RESULT_FILE_PREFIX.knbdata --name "Ubuntu $DISTRIBUTION - Kernel $KERNEL - Run $run"
-    ./knb -fd results/$RESULT_FILE_PREFIX.knbdata -o ibdbench -f results/tsv/$RESULT_FILE_PREFIX.tsv
-    ./knb -fd results/$RESULT_FILE_PREFIX.knbdata
+    RESULT_FILE_PREFIX="test_$run"
+    ./knb -v -cn worker-node -sn worker-node-2 -ot p2ptcp,p2pudp -o data -f /root/Benchmark-for-CNI/results/$RESULT_FILE_PREFIX.knbdata
+    ./knb -fd /root/Benchmark-for-CNI/results/$RESULT_FILE_PREFIX.knbdata -o ibdbench >> /root/Benchmark-for-CNI/results/csv/results.csv
+    ./knb -fd /root/Benchmark-for-CNI/results/$RESULT_FILE_PREFIX.knbdata
 done
