0d3a5a58f7c69e130ffdc5b0b3b6bc403de17a1d
mishanya363 <mihanoid369@gmail.com>
2024-03-24 23:01:22 +0300

    final results

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-for run in $(seq 1 100)
+for run in $(seq 90 91)
 do
     RESULT_FILE_PREFIX="test_$run"
     ./knb -v -cn worker-node-1 -sn worker-node-2 -ot p2ptcp,p2pudp -o ibdbench -f /root/Benchmark-for-CNI/results/csv/$RESULT_FILE_PREFIX.csv
 done
+
 res=/root/Benchmark-for-CNI/results/csv/results.csv
 if [ -e $res ]; then
         rm $res
@@ -12,7 +13,20 @@ else
         touch $res
 fi
 for run in $(seq 1 100)
+if [ -e $res ]; then
+        rm $res
+        touch $res
+else
+        touch $res
+fi
+for run in $(seq 1 100)
 do
-        awk '{print $4 " " $5 " " substr($6,12,5) " " $7 " " substr($8,12,5) " " $9 " " $10 " " substr($11,12,5) " " $12 " " substr($13,12,5)}' results/csv/test_$run.csv >> $res
+         awk '{printf $4 " " $5 " "}' test_$run.csv >> $res
+         awk -F '+' '{printf $11 " "}' test_$run.csv >> $res
+         awk '{printf $7 " "}' test_$run.csv >> $res
+         awk -F '+' '{printf $15 " "}' test_$run.csv >> $res
+         awk '{printf $9 " " $10 " "}' test_$run.csv >> $res
+         awk -F '+' '{printf $19 " "}' test_$run.csv >> $res
+         awk '{printf $12 " "}' test_$run.csv >> $res
+         awk -F '+' '{print $23}' test_$run.csv >> $res
 done
-
