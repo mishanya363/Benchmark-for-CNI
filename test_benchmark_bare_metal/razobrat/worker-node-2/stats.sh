bda94c159e3a06e96eed5ec4d93534b67e0697e0
mishanya363 <mihanoid369@gmail.com>
2024-05-26 13:29:29 +0000

    delete all bare metal bench

--- test_benchmark_bare_metal/razobrat/worker-node-2/stats.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-function now { date +%s; }
-./monit.sh > abobus
-tail -n +2 abobus | awk '{U+=$1;N+=$2;}
-                END {printf "%.2f %d\n",U/NR,N/NR}' >> results
--- test_benchmark_bare_metal/razobrat/worker-node-2/stop_iperf3.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kill -9 $(ps -aux | grep "iperf3 -s" | awk 'NR==1''{print $2}')
